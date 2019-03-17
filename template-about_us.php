9a9276c1e32276a16f8d7d82d44bb4f7897653ec
phincka <pawel@hincka.pl>
2019-03-17 13:50:56 +0100

    Main page -ready, About US - in progress

--- template-about_us.php
+++ template-about_us.php
@@ -8,18 +8,36 @@ get_header(); ?>
 
 
 
-<h1>O NAS</h1>
 
-	<?php
-		if( have_rows('about_us') ):
-		while ( have_rows('about_us') ) : the_row(); 
-	?>	
-		<div class="products_section__single">
-			<div class="product_img"><img src="<?php echo the_sub_field('about_us_picture'); ?>" alt=""></div>
-				<h2><?php echo the_sub_field('about_us_name'); ?></h2>
-					<div class="products_section__break"></div>
-						<p><?php echo the_sub_field('about_us_description'); ?></p>
+<section class="about_us">
+	<div class="about_us__top">
+		<div class="about_us__top--text">
+			<h1>Cześć</h1>
+				<h3>To my <br> <span>Ania Mukowska i Marta Tomczyńska.</span></h3>
+				<h3>Nazywamy się <b>AIM!</b></h3>
 		</div>
-	<?php endwhile; endif;?>
+		<div class="about_us__top--picture">
+			<img src="<?php asset('img/aboutus-main.png') ?>" alt="x">
+		</div>	
+	</div>
+	<div class="about_us__break">
+		<h2><?php the_field('about_us_small_title'); ?></h2>
+	</div>	
+	<div class="about_us__bottom">
+		<?php
+			if( have_rows('about_us') ):
+			while ( have_rows('about_us') ) : the_row(); 
+		?>	
+			<div class="single_person">
+				<div class="single_person--img"><img src="<?php echo the_sub_field('about_us_picture'); ?>" alt=""></div>
+					<h2 class="single_person--name"><?php echo the_sub_field('about_us_name'); ?></h2>
+						<p class="single_person--description"><?php echo the_sub_field('about_us_description'); ?></p>
+			</div>
+		<?php endwhile; endif;?>
+
+	</div>
+</section>
+
+
 
 <?php get_footer(); ?>
